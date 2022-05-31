3acf0d101c82c05759919359bcbb63a34f73a42f
小胡 <1589029587@qq.com>
2022-05-31 16:41:14 +0800

    module.exports

--- JS/4.nodejs/4.模块化/08.exports.js
+++ JS/4.nodejs/4.模块化/08.exports.js
@@ -3,11 +3,32 @@
 // console.log(module.exports)
 
 // console.log(exports === module.exports)//true
-const username = 'zs'
-module.exports.username = username
+// const username = 'zs'
+// module.exports.username = username
 
-exports.age = 20
+// exports.age = 20
 
-exports.sayHello = function () {
-    console.log('hello');
+// exports.sayHello = function () {
+//     console.log('hello');
+// }
+
+//  时刻谨记住：使用require('')导入模块时，得到的永远都是module.exports 指向的对象
+
+// module.exports = {
+//     name: 'zs',
+//     password: '123456'
+// }
+
+// exports.name = 'ls'
+
+// module.exports = {
+//     name: 'wangwu',
+//     password: '12222'
+// }
+
+exports = {
+    username: 'zs',
+    gender: '男'
 }
+module.exports = exports
+module.exports.age = 20
