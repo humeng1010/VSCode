2cfe184c7327289b1abbce3bfd936dedb23d2983
小胡 <1589029587@qq.com>
2022-07-05 20:52:05 +0800

    ajax 使用GET发送请求

--- JS/4.nodejs/5.express/13.apiRouter.js
+++ JS/4.nodejs/5.express/13.apiRouter.js
@@ -6,6 +6,7 @@ const router = express.Router()
 router.get('/get', (req, res) => {
     //通过req.query获取客户端通过查询字符串，发送到客户端的数据
     const query = req.query
+    // console.log(query);
     res.send({
         status: 0,//0 成功 1 失败
         msg: 'GET请求成功!',//状态描述
