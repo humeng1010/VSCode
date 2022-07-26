80df81f409d5ae479aa5127f7a0e0b3690f43401
小胡 <1589029587@qq.com>
2022-07-26 14:57:49 +0800

    watchEffect函数_生命周期钩子_自定义hook

--- VUE/3.VueCLI/vue3_test/11_src_vue3的生命周期/App.vue
+++ VUE/3.VueCLI/vue3_test/11_src_vue3的生命周期/App.vue
@@ -1,18 +1,15 @@
 <template>
   <button @click="changeShow">切换/隐藏</button>
   <Demo v-if="isShow"></Demo>
-  <Test></Test>
 </template>
 
 <script>
 import { ref } from "vue";
 import Demo from "./components/Demo.vue";
-import Test from "./components/Test.vue";
 export default {
   name: "App",
   components: {
     Demo,
-    Test,
   },
   setup() {
     let isShow = ref(true);
--- VUE/3.VueCLI/vue3_test/11_src_vue3的生命周期/components/Demo.vue
+++ VUE/3.VueCLI/vue3_test/11_src_vue3的生命周期/components/Demo.vue
@@ -1,24 +1,48 @@
 <template>
   <h2>当前求和为{{ sum }}</h2>
   <button @click="sum++">点击我+1</button>
-  <h2>当前鼠标点击的坐标:x {{ point.x }} y {{ point.y }}</h2>
 </template>
 
 <script>
-import { ref } from "vue";
-import usePoint from "../hooks/usePoint";
+import {
+  onBeforeMount,
+  onBeforeUnmount,
+  onBeforeUpdate,
+  onMounted,
+  onUnmounted,
+  onUpdated,
+  ref,
+} from "vue";
 export default {
   name: "Demo",
   //setup的创建时机比beforeCreate早  而且可以接收到的两个参数 props context
   setup() {
     // 数据
     let sum = ref(0);
-    let point = usePoint();
+    console.log("setup");
 
+    // 通过组合API的形式使用生命周期钩子
+    onBeforeMount(() => {
+      console.log("onBeforeMount");
+    });
+    onMounted(() => {
+      console.log("onMounted");
+    });
+    onBeforeUpdate(() => {
+      console.log("onBeforeUpdate");
+    });
+    onUpdated(() => {
+      console.log("onUpdated");
+    });
+    onBeforeUnmount(() => {
+      console.log("onBeforeUnmount");
+    });
+    onUnmounted(() => {
+      console.log("onUnmounted");
+    });
     // 返回一个对象(常用)
     return {
       sum,
-      point,
     };
   },
 
