5c42ccf8d1e3625b8c259855a617bfa1c2a53fb4
小胡 <1589029587@qq.com>
2022-07-28 13:33:15 +0800

    customRef

--- VUE/3.VueCLI/vue3_test/src/App.vue
+++ VUE/3.VueCLI/vue3_test/src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <button @click="changeShow">切换/隐藏</button>
-  <Demo v-if="isShow"></Demo>
+  <input type="text" v-model="keyword" />
+  <h3>{{ keyword }}</h3>
 </template>
 
 <script>
 import { ref } from "vue";
-import Demo from "./components/Demo.vue";
 export default {
   name: "App",
-  components: {
-    Demo,
-  },
   setup() {
-    let isShow = ref(true);
-
-    function changeShow() {
-      isShow.value = !isShow.value;
+    // 自定义一个ref
+    function myRef(value) {
+      console.log("---myRef---", value);
     }
+    // let keyword = ref("hello"); //使用vue提供的ref
+    let keyword = myRef("hello"); //使用程序员自定义的ref
 
     return {
-      isShow,
-      changeShow,
+      keyword,
     };
   },
 };
