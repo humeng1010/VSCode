c286062a75834e57ef05afb093459f32bf6fde57
小胡 <1589029587@qq.com>
2022-07-28 13:39:11 +0800

    customRef

--- VUE/3.VueCLI/vue3_test/src/App.vue
+++ VUE/3.VueCLI/vue3_test/src/App.vue
@@ -4,13 +4,22 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, customRef } from "vue";
 export default {
   name: "App",
   setup() {
     // 自定义一个ref
     function myRef(value) {
       console.log("---myRef---", value);
+      return customRef((track, trigger) => {
+        return {
+          get() {
+            track();
+            return value;
+          },
+          set() {},
+        };
+      });
     }
     // let keyword = ref("hello"); //使用vue提供的ref
     let keyword = myRef("hello"); //使用程序员自定义的ref
